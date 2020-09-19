3c00dd12d0c913295510f1656aa905ed4ee1ead0
ChristosKrilisDev <christos.krilis.dev@gmail.com>
2020-09-19 23:18:45 +0300

    Upload Scripts
    Uploading Only Scripts Files

---
+++ Scripts/Camera/CameraController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField] private Transform _target;
+    [SerializeField] private Transform _pivot;
+    //Transform _parentTarget;
+    [SerializeField] private Vector3 _offset;
+    [SerializeField] private bool _useOffset;
+    [SerializeField] bool _invertYMouse = false;
+    [Range(-1,1)]int _invertMouseValue = 1;
+
+    [SerializeField][Range(0f,1f)] private float _lerpDelay;
+
+    [SerializeField] private float _cRotateSpeed;
+
+    [Header("View Angles Vars")]
+    [SerializeField] float _maxViewAngle = 45f;
+    [SerializeField] float _minViewAgnle = -45f;
+
+
+
+    void Start()
+    {
+        //_offset = _target.position - transform.position;
+        //_parentTarget = _target.parent;
+        if(_invertYMouse)
+            _invertMouseValue = -1;
+
+        if(!_useOffset)
+            _offset = _target.position - transform.position;
+
+        _pivot.transform.position = _target.transform.position;
+        _pivot.transform.parent = _target.transform;
+    }
+
+
+    void LateUpdate()
+    {
+        if(_target == null)
+            return;
+
+        //Lock mouse , Hide mouse
+        MouseCursorActivity();
+
+        //Get the x position of the mouse & rotate the target 
+        float horizontal = Input.GetAxis("Mouse X") * _cRotateSpeed;
+        _target.Rotate(0 , horizontal , 0);
+
+        //get the y position of the mouse & rotate the @pivot
+        float vertical = Input.GetAxis("Mouse Y") * _cRotateSpeed;
+        //transform.Rotate(_invertMouseValue * vertical , 0 , 0);
+        _pivot.Rotate(vertical , 0 , 0);
+
+        //Invert Y
+        //if(_invertYMouse)
+        //    _pivot.Rotate(vertical , 0 , 0);
+        //else
+        //    _pivot.Rotate(-vertical , 0 , 0);
+
+        //Limit the camera up/down rotation
+        if(_pivot.rotation.eulerAngles.x > _maxViewAngle && _pivot.rotation.eulerAngles.x < 180f)
+            _pivot.rotation = Quaternion.Euler(_maxViewAngle , 0 , 0);
+        if(_pivot.rotation.eulerAngles.x > 180 && _pivot.rotation.eulerAngles.x < 360f + _minViewAgnle)
+            _pivot.rotation = Quaternion.Euler(360f + _minViewAgnle , 0 , 0);
+
+
+        //move the camera based on the current rotation and the original offset
+        float desiredYangle = _target.eulerAngles.y;
+        //float desiredXangle = transform.eulerAngles.x;
+        float desiredXangle = _pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredXangle , desiredYangle , 0);
+
+
+
+        //Move/rotate camera here
+        transform.position = _target.position - (rotation * _offset);
+
+        if(transform.position.y < _target.position.y)
+            transform.position = new Vector3(transform.position.x , _target.transform.position.y - 0.5f , transform.position.z);
+
+
+        transform.LookAt(_target);
+    }
+
+    private void FollowTarget()
+    {
+        //transform.position = _target.position + _offset;
+        //transform.position = Vector3.Lerp(transform.position , _target.transform.position + _offset , _lerpDelay * Time.deltaTime);
+        
+    }
+
+
+    private bool isMouseUnlokced = false;
+    #region Test
+    private void MouseCursorActivity()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isMouseUnlokced = !isMouseUnlokced;
+
+            Cursor.visible = isMouseUnlokced;
+
+            if(!isMouseUnlokced)
+                Cursor.lockState = CursorLockMode.Locked;
+            else
+                Cursor.lockState = CursorLockMode.None;
+        }
+    }
+    #endregion
+
+
+}
---
+++ Scripts/Player/MovementController.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/// <summary>
+/// About Movement Controller
+/// Controll player movement from this script
+/// Controll Physics
+/// 
+/// RULES:
+/// While P(w) or P(s) => turnLimits := -45 , 45 , LootAt : Forward || Backward
+/// So the player is moving in a |45| (max) nDegree but he is looking Forward || Backward
+/// While Moving : Camera.Rotate.Player
+/// 
+/// </summary>
+/// 
+
+
+[RequireComponent(typeof(CharacterController))]
+public class MovementController : MonoBehaviour
+{
+    //Movement
+    [Header("Movment Members")]
+    [SerializeField] private float _movementSpeed = 5;
+    private Vector3 _moveDirection;
+    [SerializeField]private Animator _playerAnimator;
+
+    [Space]
+    [Header("Slide Members")]
+    [SerializeField] private float slideSpeed = 20f; // slide speed
+    private bool  isSliding  = false;
+    private Vector3  slideForward ; // direction of slide
+    private float  slideTimer = 0.0f;
+    [SerializeField]private float slideTimerMax = 2.5f; // time while sliding
+
+    //Gravity
+    [Space]
+    [Header("Physics")]
+    [SerializeField] private float _gravityScale = 1.25f;
+    [SerializeField] private float _jumpForce = 5;
+    bool _hasDoubleJump = false;
+
+
+    private CharacterController _controller;
+
+    [Space]
+    [Header("FX")]
+    public GameObject _fx;
+    public Transform _spawnFxPoint;
+
+
+    private void Start()
+    {
+        //_playerAnimator = FindObjectOfType<Animator>();
+        _controller = FindObjectOfType<CharacterController>();
+    }
+
+    float _runSpeed = 1;
+    bool isRunning;
+
+    private void Update()
+    {
+
+        UpdateMovementInput();
+
+        SlideMovement();
+        PlayerJump();
+
+        //Move player/ update anims
+
+        MovePlayer();
+       // UpdateWalkAnim();
+    }
+    void UpdateMovementInput()
+    {
+        //Set new direction to move
+        //_moveDirection = new Vector3(Input.GetAxis("Horizontal") * _movementSpeed , /*_controller.velocity.y*/ _moveDirection.y , Input.GetAxis("Vertical") * _movementSpeed);
+        if(Input.GetKey(KeyCode.LeftShift) && _controller.isGrounded)
+        {
+            _runSpeed = _movementSpeed / 2.5f;
+            isRunning = true;
+        }
+        else
+        {
+            _runSpeed = 1f;
+            isRunning = false;
+        }
+
+        float yStore = _moveDirection.y;
+
+        _moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+        _moveDirection = _moveDirection.normalized * _runSpeed * _movementSpeed;
+
+        _moveDirection.y = yStore;
+    }
+
+
+    void SlideMovement()
+    {
+        var h = _controller.height;
+        //-slide -
+        if(Input.GetKeyDown(KeyCode.F) && !isSliding) // press F to slide
+        {
+            slideTimer = 0.0f; // start timer
+            isSliding = true;
+            slideForward = transform.forward;
+        }
+        if(isSliding)
+        {
+            h = 0.5f * _controller.height; // height is crouch height
+            _finalSpeed = slideSpeed; // speed is slide speed
+            _moveDirection = slideForward * _movementSpeed*2;
+
+          //  if(transform.rotation.eulerAngles.y >= -60)
+          //      transform.rotation = Mathf.Lerp ( transform.rotation.eulerAngles, Quaternion.Euler(1f,60f,1f),.25f);
+
+            slideTimer += Time.deltaTime;
+
+            if(slideTimer > slideTimerMax)
+            {
+                isSliding = false;
+            }
+        }
+
+        _controller.height = Mathf.Lerp(_controller.height , h , 5 * Time.deltaTime);
+    }
+
+    private float _finalSpeed;
+
+    void MovePlayer()
+    {
+        _moveDirection.y += Physics.gravity.y * _gravityScale * Time.deltaTime;
+        //Finally move
+        _controller.Move(_moveDirection * Time.deltaTime);
+    }
+
+    void PlayerJump()
+    {
+        if(_controller.isGrounded)
+        {
+            //Prevent the massive dicrease of direction.y, 
+            //so set it 0 each time to avoid ulta gravity on fall from a hight
+            _moveDirection.y = 0;
+            _hasDoubleJump = true;
+
+            if(Input.GetButtonDown("Jump"))
+            {
+                //UpdateJumpAnims();
+
+                _moveDirection.y = _jumpForce;
+                //Jump Anims
+            }
+
+        }
+        //Perform double jump , if the player isnt touching the ground 
+        //and is available to Double jump
+        if(!_controller.isGrounded)
+            if(_hasDoubleJump)
+                if(Input.GetButtonDown("Jump"))
+                {
+                    _hasDoubleJump = false;
+
+                    //Create jump FX
+                    GameObject newFx = Instantiate(_fx);
+                    newFx.transform.position = _spawnFxPoint.position;
+                    //newFx.transform.localPosition = Vector3.zero;
+
+                    _moveDirection.y = _jumpForce + _jumpForce / 2f;
+                }
+    }
+
+
+
+
+    private void FixedUpdate()
+    {
+        //add default gravity
+           
+
+    }
+
+
+    private void UpdateWalkAnim()
+    {
+        //Move Anims
+        if(_moveDirection.magnitude > 1.5f && _controller.isGrounded)
+            if(isRunning)
+            {
+                _playerAnimator.SetBool("IsRunning" , isRunning);
+            }
+            else
+            {
+                _playerAnimator.SetBool("IsRunning" , false);
+                _playerAnimator.SetBool("IsWalking" , true);
+            }
+
+        else
+            _playerAnimator.SetBool("IsWalking" , false);
+
+
+
+        _playerAnimator.SetBool("IsGrounded" , _controller.isGrounded);
+    }
+
+    private void UpdateJumpAnims()
+    {
+        if(isRunning)
+        {
+            _playerAnimator.SetTrigger("RunningJump");
+        }
+        else if(_moveDirection.magnitude <= 1.5f)
+        {
+            _playerAnimator.SetTrigger("StaticJump");
+        }
+        else if(_moveDirection.magnitude >= 3.5f)
+        {
+            _playerAnimator.SetTrigger("WalkJump");
+        }
+
+    }
+
+}
+
+//var walkSpeed: float = 7; // regular speed
+// var crchSpeed: float = 3; // crouching speed
+// var runSpeed: float = 20; // run speed
+ 
+// private var chMotor: CharacterMotor;
+// private var ch: CharacterController;
+// private var tr: Transform;
+// private var height: float; // initial height
+ 
+// public var slideSpeed: float = 20; // slide speed
+// private var isSliding : boolean = false;
+// private var slideForward : Vector3; // direction of slide
+// private var slideTimer : float = 0.0;
+// public var slideTimerMax : float = 2.5; // time while sliding
+ 
+// function Start()
+//{
+//    chMotor = GetComponent(CharacterMotor);
+//    tr = transform;
+//    ch = GetComponent(CharacterController);
+//    height = ch.height;
+//}
+
+//function Update()
+//{
+//    var h = height;
+//    var speed = walkSpeed;
+
+//    // - run and crouch -    
+//    if(ch.isGrounded && Input.GetKey("left shift") || Input.GetKey("right shift")) // press Shift to run
+//    {
+//        speed = runSpeed;
+//    }
+//    if(Input.GetKey("c")) // press C to crouch
+//    {
+//        h = 0.5 * height;
+//        speed = crchSpeed; // slow down when crouching
+//    }
+
+//    // - slide -    
+//    if(Input.GetKeyDown("f") && !isSliding) // press F to slide
+//    {
+//        slideTimer = 0.0; // start timer
+//        isSliding = true;
+//        slideForward = tr.forward;
+//    }
+//    if(isSliding)
+//    {
+//        h = 0.5 * height; // height is crouch height
+//        speed = slideSpeed; // speed is slide speed
+//        chMotor.movement.velocity = slideForward * speed;
+
+//        slideTimer += Time.deltaTime;
+//        if(slideTimer > slideTimerMax)
+//        {
+//            isSliding = false;
+//        }
+//    }
+
+//    // - apply movement modifiers -    
+//    chMotor.movement.maxForwardSpeed = speed; // set max speed
+//    var lastHeight = ch.height; // crouch/stand up smoothly 
+//    ch.height = Mathf.Lerp(ch.height , h , 5 * Time.deltaTime);
+//    tr.position.y += (ch.height - lastHeight) / 2; // fix vertical position
+//}
